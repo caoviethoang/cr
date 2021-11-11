b1137555166a7ccd9b2014e6cf0e0a8d1f37c1f4
caoviethoang <hoangcv17@gmail.com>
2021-11-11 11:03:52 +0700

    h

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Kiều Anh này !',
+    introDesc: `Anh có điều này muốn nói với em ! `,
+    btnIntro: 'oke',
+    title: 'Anh biết là hơi đột ngột, nhưng anh nghĩ là nó cần thiết. Lời yêu thương của anh không còn giống những năm mười mấy tuổi, chỉ là, những hôm làm khuya, được trò chuyện với em là không còn mệt mỏi căng thẳng gì nữa hết. Tối qua mơ thấy em, sáng nay tỉnh dậy, điều đầu tiên anh nghĩ đến là em, nhất định hôm nay sẽ là 1 ngày tuyệt vời !  Anh có thể nắm tay, yêu thương, che chở cho em và cùng bước đi trên đoạn đường phía trước không ?',
+    desc: 'Trả lời anh nhé ! :> ',
+    btnYes: 'Em đồng ý',
+    btnNo: '...',
+    question:'Có muốn nói với anh điều gì không :)))',
+    btnReply: 'Gửi cho anh',
+    reply: 'Em cũng thích anh !',
+    mess: 'Cảm ơn em <3',
+    messDesc: 'Cuối tuần này đi chơi nhé =))',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/hoangcv17/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
